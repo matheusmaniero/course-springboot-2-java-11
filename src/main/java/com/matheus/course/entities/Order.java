f9cee539faa5ba6ef8ca089bb22d00b94eac1013
Matheus Maniero <matheusmaniero@gmail.com>
2022-06-11 13:09:19 -0300

    OrderStatus enum

--- src/main/java/com/matheus/course/entities/Order.java
+++ src/main/java/com/matheus/course/entities/Order.java
@@ -13,37 +13,52 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.matheus.course.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
-	
+
 	private User client;
-	
+
+	private Integer orderStatus;
+
 	public Order() {
-		
+
 	}
 
-	public Order(Long id, Instant moment, User client) {		
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		this.setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
 		return id;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+
 	}
 
 	public void setId(Long id) {
@@ -82,10 +97,5 @@ public class Order implements Serializable {
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
 
 }
