ad5ae4f4e899a766b9f4908981c3a3616b32b831
Matheus Maniero <matheusmaniero@gmail.com>
2022-06-13 15:54:19 -0300

    Payment, one-to-one association

--- src/main/java/com/matheus/course/config/TestConfig.java
+++ src/main/java/com/matheus/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.matheus.course.entities.Category;
 import com.matheus.course.entities.Order;
 import com.matheus.course.entities.OrderItem;
+import com.matheus.course.entities.Payment;
 import com.matheus.course.entities.Product;
 import com.matheus.course.entities.User;
 import com.matheus.course.enums.OrderStatus;
@@ -65,7 +66,7 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.CANCELED, u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.SHIPPED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 
@@ -78,6 +79,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-07-21T08:42:10Z"), o2 );
+		o2.setPayment(pay1);
+		orderRepository.save(o2);
 	}
 
 }
