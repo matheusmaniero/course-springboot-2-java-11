9148911ce457bae3e54f379893f307fc74fb73f7
Matheus Maniero <matheusmaniero@gmail.com>
2022-06-13 09:06:39 -0300

    Product

--- src/main/java/com/matheus/course/entities/Category.java
+++ src/main/java/com/matheus/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.matheus.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name="tb_category")
@@ -19,6 +22,9 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 		
@@ -35,6 +41,10 @@ public class Category implements Serializable {
 
 	public String getName() {
 		return name;
+	}
+	
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
@@ -53,9 +63,5 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
 
 }
